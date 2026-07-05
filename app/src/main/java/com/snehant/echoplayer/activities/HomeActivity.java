daabf6036e32fa01b4061d2fa2b2988a98b2358c
ap1339 <arehaant1235@gmail.com>
2026-07-05 22:15:49 +0530

    Sprint 1.2 - Added Splash Screen and Home Activity

--- app/src/main/java/com/snehant/echoplayer/activities/HomeActivity.java
+++ app/src/main/java/com/snehant/echoplayer/activities/HomeActivity.java
@@ -1,4 +1,16 @@
 package com.snehant.echoplayer.activities;
 
-public class HomeActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.snehant.echoplayer.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 }
--- app/src/main/java/com/snehant/echoplayer/activities/SplashActivity.java
+++ app/src/main/java/com/snehant/echoplayer/activities/SplashActivity.java
@@ -1,4 +1,30 @@
 package com.snehant.echoplayer.activities;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.snehant.echoplayer.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+
+            Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+            startActivity(intent);
+
+            finish();
+
+        }, SPLASH_DELAY);
+    }
 }
